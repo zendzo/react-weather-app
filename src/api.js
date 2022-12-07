0de61faa8fb2e4e556c33922f6553901bd80bf28
M Zaenal Mustofa <zendzzo@gmail.com>
2022-12-07 10:54:46 +0700

    Work Done

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "5752177fe2mshd8af432a1e0b7b6p1db31djsn5e2fc6517757",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "0d8d45d645229a5d4b9c6a33775ab8a3";
+
---
+++ src/components/forecast/forecast.js
@@ -0,0 +1,88 @@
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemButton,
+  AccordionItemHeading,
+  AccordionItemPanel,
+} from "react-accessible-accordion";
+import "./forecast.css";
+
+const WEEK_DAYS = [
+  "Senin",
+  "Selasa",
+  "Rabu",
+  "Kamis",
+  "Jumat",
+  "Sabtu",
+  "Minggu",
+];
+
+const Forecast = ({ data }) => {
+  const dayInWeek = new Date().getDay();
+  const forecastDays = WEEK_DAYS.slice(dayInWeek, WEEK_DAYS.length).concat(
+    WEEK_DAYS.slice(0, dayInWeek)
+  );
+
+  console.log(forecastDays);
+  console.log(dayInWeek);
+  return (
+    <>
+      <label className="title">Daily</label>
+      <Accordion allowZeroExpanded>
+        {data.list.slice(0, 7).map((item, index) => (
+          <AccordionItem key={index}>
+            <AccordionItemHeading>
+              <AccordionItemButton>
+                <div className="daily-item">
+                  <img
+                    src={`icons/${item.weather[0].icon}.png`}
+                    alt="weather"
+                    className="icon-small"
+                  />
+                  <label className="day">{forecastDays[index]}</label>
+                  <label className="description">
+                    {item.weather[0].description}
+                  </label>
+                  <label className="min-max">
+                    {Math.round(item.main.temp_min)}°C /
+                    {Math.round(item.main.temp_max)}°C
+                  </label>
+                </div>
+              </AccordionItemButton>
+            </AccordionItemHeading>
+            <AccordionItemPanel>
+              <div className="daily-details-grid">
+                <div className="daily-details-grid-item">
+                  <label>Pressure</label>
+                  <label>{item.main.pressure} hPa</label>
+                </div>
+                <div className="daily-details-grid-item">
+                  <label>Humidity</label>
+                  <label>{item.main.humidity}%</label>
+                </div>
+                <div className="daily-details-grid-item">
+                  <label>Clouds</label>
+                  <label>{item.clouds.all}</label>
+                </div>
+                <div className="daily-details-grid-item">
+                  <label>Wind Speed</label>
+                  <label>{item.wind.speed}m/s</label>
+                </div>
+                <div className="daily-details-grid-item">
+                  <label>Sea Level</label>
+                  <label>{item.main.sea_level}m</label>
+                </div>
+                <div className="daily-details-grid-item">
+                  <label>Feels Like</label>
+                  <label>{item.main.feels_like}°C</label>
+                </div>
+              </div>
+            </AccordionItemPanel>
+          </AccordionItem>
+        ))}
+      </Accordion>
+    </>
+  );
+};
+
+export default Forecast;
